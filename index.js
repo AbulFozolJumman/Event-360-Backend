763d7ebc91d418ed070f2d97a6ec747d9193f75a
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-02-08 00:51:13 +0600

    Get and create event coding done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
@@ -7,6 +8,46 @@ const port = process.env.PORT;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// MongoDB uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ztfqf2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // DB collections
+    const eventsCollection = client.db("Event-360").collection("events");
+
+    // Get all events
+    app.get("/events", async (req, res) => {
+      const allEvents = await eventsCollection.find().toArray();
+      res.send(allEvents);
+    });
+
+    // Add a event
+    app.post("/events", async (req, res) => {
+      const addedEvent = req.body;
+      const result = await eventsCollection.insertOne(addedEvent);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Event 360 backend is running...");
